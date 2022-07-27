038e01b36a39385d1b821f07dcc14cbd915b4048
itayle23-meet <109908182+itayle23-meet@users.noreply.github.com>
2022-07-27 12:39:38 +0300

    done

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,17 +8,28 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
+username = "itay"
 password = "123"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/' , methods = ['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		username = request.form['username']
+		password = request.form['password']
+		if username == "itay" and password == '123':
+			return render_template('home.html', u = username, p = password , facebook_friends = facebook_friends)
   
+@app.route('/home')
+def home():
+	return render_template('home.html' , facebook_friends = facebook_friends)
 
-
+@app.route('/friend_exists', methods = ['GET', 'POST'])
+def friend_exists():
+	return render_template('friend_exists.html')
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
